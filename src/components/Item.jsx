4546d0b52d096100909c41450f0c855f60b16e39
Alexandar Valov <alexvulov123@gmail.com>
2023-04-05 16:10:37 +0300

    Implemented an intersection observer with both the help of useInView hook from react-intersection-observer package and without it. The part that is not commented is the one without the hook, so the difference between the two methods of implementing such a feature could be seen.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AiFillHeart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 
 const Item = ({ item }) => {
+  const { ref: myRef, inView: myElementIsVisible } = useInView();
+
+  //  Intersection Observer's functionallity without the hook
+
+  // const myRef = useRef();
+  // const [myElementIsVisible, setMyElementIsVisible] = useState();
+  // console.log(myElementIsVisible);
+  // useEffect(() => {
+  //   // console.log("myRef", myRef.current);
+  //   const observer = new IntersectionObserver((entries) => {
+  //     const entry = entries[0];
+  //     setMyElementIsVisible(entry.isIntersecting);
+  //     // console.log("entry", entry);
+  //   });
+  //   observer.observe(myRef.current);
+  // }, []);
+
   return (
     <Link
+      ref={myRef}
       to={"/watches/" + item.id}
-      className="flex rounded-sm justify-center items-center bg-white shadow-md h-80 group cursor-pointer relative"
+      className={
+        myElementIsVisible
+          ? "flex rounded-sm justify-center items-center bg-white shadow-md h-80 group cursor-pointer relative animate-animateOpacity"
+          : "flex rounded-sm justify-center items-center bg-white shadow-md h-80 group cursor-pointer relative"
+      }
     >
       <div className="absolute top-2 left-2 w-8 h-8 rounded-full flex justify-center items-center bg-[#dddddd] hover:bg-green-800 transition-all">
         <AiFillHeart
