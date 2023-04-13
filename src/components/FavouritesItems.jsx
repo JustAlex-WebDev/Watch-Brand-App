5dd91afd021a9b86bd6b63b5a0206d6b220057c4
Alexandar Valov <alexvulov123@gmail.com>
2023-04-13 22:04:58 +0300

    Updated the Favourites Section design and the UI of the Items stored there too. Also added a new, more interesting page transitioning that is not fully done. However, it soon be fully finished.

--- src/components/FavouritesItems.jsx
+++ src/components/FavouritesItems.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+import { AiOutlineClose, AiOutlineInfoCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const FavouritesItems = ({ item, isInFavSection, removeItem }) => {
+const FavouritesItems = ({
+  item,
+  isInFavSection,
+  removeItem,
+  setFav,
+  goToPrev,
+}) => {
   return (
-    <div className="w-[20%] flex flex-col justify-center items-center">
-      <img
-        className="w-auto h-48 max-h-48 group-hover:scale-105 transition-all duration-300 cursor-pointer"
-        src={item.img}
-        alt=""
-      />
+    <div className="w-[20%] flex justify-center items-center z-10">
+      <div className="relative group">
+        <img
+          className="w-auto h-56 max-h-56 group-hover:scale-105 transition-all duration-300"
+          src={item.img}
+          alt=""
+        />
+        <div className="absolute hover:backdrop-blur-sm top-0 left-0 w-full h-full hidden group-hover:block transition-all">
+          <div className="flex justify-center items-center gap-4 h-full font-bold">
+            <Link to={"/watches/" + item.id} onClick={() => setFav(false)}>
+              <AiOutlineInfoCircle
+                size={32}
+                title="Info"
+                className="cursor-pointer text-green-800"
+              />
+            </Link>
+            <div
+              onClick={() => removeItem(item.id) & goToPrev()}
+              className="rounded-full border border-black p-1"
+            >
+              <AiOutlineClose
+                size={18}
+                title="Remove"
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/Watches.jsx
+++ src/routes/Watches.jsx
@@ -4,8 +4,9 @@ import Footer from "../components/Footer";
 import { watches } from "../data";
 import Item from "../components/Item";
 import { useFavSection } from "../context/FavContext";
+import PageTransition from "../components/PageTransition";
 
- // Local Storage Test
+// Local Storage Test
 // const favouritesData = JSON.parse(
 //   window.localStorage.getItem("MY_FAVOURITES_STATE")
 // );
@@ -31,71 +32,74 @@ const Watches = () => {
   // }, [favourites]);
 
   return (
-    <m.div
-      initial={{ y: "100%" }}
-      animate={{ y: "0%" }}
-      transition={{ duration: 0.75, ease: "easeOut" }}
-      exit={{ opacity: 1 }}
-      className="text-grey-900 bg-[#fff] absolute top-0 left-0 w-full lg:px-48 px-16"
-    >
-      <main className="main-div mb-64">
-        <div className="mt-48 mb-16 p-2">
-          <m.h1
+    <>
+      <PageTransition />
+      <m.div
+        initial={{ y: "100%" }}
+        animate={{ y: "0%" }}
+        transition={{ duration: 0.75, delay: 3, ease: "easeOut" }}
+        exit={{ opacity: 1 }}
+        className="text-grey-900 absolute top-0 left-0 w-full h-full bg-[#fff] lg:px-48 px-16"
+      >
+        <div className="main-div mb-64">
+          <div className="mt-48 mb-16 p-2">
+            <m.h1
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 3.75, duration: 1 }}
+              className="text-3xl lg:text-4xl text-center tracking-widest"
+            >
+              TIMELESS WATCHES
+            </m.h1>
+          </div>
+
+          <m.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ delay: 0.5, duration: 1 }}
-            className="text-3xl lg:text-4xl text-center tracking-widest"
+            transition={{ delay: 4, duration: 1 }}
+            className="flex justify-center items-center mb-12 gap-8 font-bold cursor-pointer"
           >
-            TIMELESS WATCHES
-          </m.h1>
+            <div className="flex justify-center items-center gap-2">
+              <div className="text-green-800 hover:text-green-800">All</div>
+              <div className="w-2 h-2 bg-green-800 rounded-full"></div>
+            </div>
+            <div>
+              <div className="hover:text-green-800">Classic</div>
+            </div>
+            <div>
+              <div className="hover:text-green-800">Professional</div>
+            </div>
+          </m.div>
+
+          <m.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 4.25, duration: 1 }}
+            className="mt-16 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-2 justify-between items-center w-full sm:w-[80%] m-auto p-2"
+          >
+            {items.map((item) => {
+              const isInFavSection = favSection.some((i) => i.id === item.id);
+              return (
+                <Item
+                  item={item}
+                  isInFavSection={isInFavSection}
+                  addItem={addItem}
+                  removeItem={removeItem}
+                  key={item.id}
+                />
+              );
+            })}
+          </m.div>
         </div>
-
         <m.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ delay: 0.75, duration: 1 }}
-          className="flex justify-center items-center mb-12 gap-8 font-bold cursor-pointer"
+          transition={{ delay: 4.5, duration: 1 }}
         >
-          <div className="flex justify-center items-center gap-2">
-            <div className="text-green-800 hover:text-green-800">All</div>
-            <div className="w-2 h-2 bg-green-800 rounded-full"></div>
-          </div>
-          <div>
-            <div className="hover:text-green-800">Classic</div>
-          </div>
-          <div>
-            <div className="hover:text-green-800">Professional</div>
-          </div>
+          <Footer />
         </m.div>
-
-        <m.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1, duration: 1 }}
-          className="mt-16 grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-2 justify-between items-center w-full sm:w-[80%] m-auto p-2"
-        >
-          {items.map((item) => {
-            const isInFavSection = favSection.some((i) => i.id === item.id);
-            return (
-              <Item
-                item={item}
-                isInFavSection={isInFavSection}
-                addItem={addItem}
-                removeItem={removeItem}
-                key={item.id}
-              />
-            );
-          })}
-        </m.div>
-      </main>
-      <m.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.75, duration: 1 }}
-      >
-        <Footer />
       </m.div>
-    </m.div>
+    </>
   );
 };
 
